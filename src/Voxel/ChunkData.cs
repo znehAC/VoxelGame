acb0cc2d97ccd29e7f0551532793471f33c81e05
znehAC <adrianocesar321@gmail.com>
2025-09-25 20:53:50 -0300

    Revert Async Mesh Gen

--- src/Voxel/ChunkData.cs
+++ src/Voxel/ChunkData.cs
@@ -1,36 +1,20 @@
 using Godot;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 public partial class ChunkData : GodotObject
 {
     public const int Size = 32;
-    private const int PaddedSize = Size + 2;
-    public Godot.Vector3I Position { get; private set; }
+    public Vector3I Position { get; private set; }
 
     private readonly World _world;
     private readonly byte[] _voxels = new byte[Size * Size * Size];
+
 
     private MeshInstance3D _meshInstance;
     private const byte Air = 0;
     private const byte Dirt = 1;
     private const byte Stone = 2;
 
-    private readonly struct ThreadSafeMeshResult
-    {
-        public readonly List<System.Numerics.Vector3> Vertices;
-        public readonly List<System.Numerics.Vector3> Normals;
-        public readonly List<System.Drawing.Color> Colors;
-
-        public ThreadSafeMeshResult(List<System.Numerics.Vector3> v, List<System.Numerics.Vector3> n, List<System.Drawing.Color> c)
-        {
-            Vertices = v;
-            Normals = n;
-            Colors = c;
-        }
-    }
-
-    public ChunkData(World world, Godot.Vector3I position)
+    public ChunkData(World world, Vector3I position)
     {
         _world = world;
         this.Position = position;
@@ -44,172 +28,153 @@ public partial class ChunkData : GodotObject
         parent.AddChild(_meshInstance);
     }
 
-    private bool _isMeshing = false;
-
-    public async Task RebuildMesh()
+    public void RebuildMesh()
     {
-        if (_isMeshing) return;
-        _isMeshing = true;
-
-        try
-        {
-            byte[] paddedVoxels = new byte[PaddedSize * PaddedSize * PaddedSize];
-            for (int x = 0; x < PaddedSize; x++)
-            {
-                for (int y = 0; y < PaddedSize; y++)
-                {
-                    for (int z = 0; z < PaddedSize; z++)
-                    {
-                        paddedVoxels[(y * PaddedSize * PaddedSize) + (z * PaddedSize) + x] =
-                            GetVoxel(x - 1, y - 1, z - 1);
-                    }
-                }
-            }
-
-            ThreadSafeMeshResult meshData = await Task.Run(() => GenerateMeshData(paddedVoxels));
-            Callable.From(() => ApplyMeshData(meshData)).CallDeferred();
-        }
-        catch (System.Exception e)
-        {
-            GD.PrintErr($"Meshing error in chunk {Position}: {e.Message}");
-            _isMeshing = false;
-        }
+        if (_meshInstance == null) return;
+        _meshInstance.Mesh = GenerateMesh();
     }
 
-    private void ApplyMeshData(ThreadSafeMeshResult meshData)
+
+    private ArrayMesh GenerateMesh()
     {
-        if (_meshInstance != null)
+        var st = new SurfaceTool();
+        st.Begin(Mesh.PrimitiveType.Triangles);
+
+        var axes = new[] { Vector3I.Axis.X, Vector3I.Axis.Y, Vector3I.Axis.Z };
+
+        foreach (var axis in axes)
         {
-            var newMesh = new ArrayMesh();
-            if (meshData.Vertices.Count > 0)
-            {
-                var godotVertices = new Godot.Collections.Array<Godot.Vector3>();
-                var godotNormals = new Godot.Collections.Array<Godot.Vector3>();
-                var godotColors = new Godot.Collections.Array<Godot.Color>();
+            int uAxis = ((int)axis + 1) % 3;
+            int vAxis = ((int)axis + 2) % 3;
 
-                foreach (var v in meshData.Vertices) godotVertices.Add(new Godot.Vector3(v.X, v.Y, v.Z));
-                foreach (var n in meshData.Normals) godotNormals.Add(new Godot.Vector3(n.X, n.Y, n.Z));
-                foreach (var c in meshData.Colors) godotColors.Add(new Godot.Color(c.R / 255f, c.G / 255f, c.B / 255f, c.A / 255f));
-
-                var arrays = new Godot.Collections.Array();
-                arrays.Resize((int)Mesh.ArrayType.Max);
-                arrays[(int)Mesh.ArrayType.Vertex] = godotVertices;
-                arrays[(int)Mesh.ArrayType.Normal] = godotNormals;
-                arrays[(int)Mesh.ArrayType.Color] = godotColors;
-                newMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, arrays);
-            }
-            _meshInstance.Mesh = newMesh;
-        }
-        _isMeshing = false;
-    }
-
-    private static ThreadSafeMeshResult GenerateMeshData(byte[] paddedVoxels)
-    {
-        var vertices = new List<System.Numerics.Vector3>();
-        var normals = new List<System.Numerics.Vector3>();
-        var colors = new List<System.Drawing.Color>();
-
-        for (int axis = 0; axis < 3; ++axis)
-        {
-            int uAxis = (axis + 1) % 3;
-            int vAxis = (axis + 2) % 3;
-            var dir = new int[3];
-            dir[axis] = 1;
             var x = new int[3];
-            var mask = new byte[Size, Size];
+            var dir = new Vector3I();
+            dir[(int)axis] = 1;
 
-            for (x[axis] = 0; x[axis] < Size; ++x[axis])
+            // A mask to hold the voxel type for each face on the current slice
+            var mask = new byte[ChunkData.Size, ChunkData.Size];
+
+            // Sweep along the current axis
+            for (x[(int)axis] = -1; x[(int)axis] < ChunkData.Size;)
             {
-                for (x[vAxis] = 0; x[vAxis] < Size; ++x[vAxis])
+                // 1. BUILD THE MASK for the current slice
+                for (x[vAxis] = 0; x[vAxis] < ChunkData.Size; ++x[vAxis])
                 {
-                    for (x[uAxis] = 0; x[uAxis] < Size; ++x[uAxis])
+                    for (x[uAxis] = 0; x[uAxis] < ChunkData.Size; ++x[uAxis])
                     {
-                        int p1_x = x[0] + 1; int p1_y = x[1] + 1; int p1_z = x[2] + 1;
-                        int p2_x = p1_x + dir[0]; int p2_y = p1_y + dir[1]; int p2_z = p1_z + dir[2];
+                        byte type1 = GetVoxel(x[0], x[1], x[2]);
+                        byte type2 = GetVoxel(x[0] + dir.X, x[1] + dir.Y, x[2] + dir.Z);
 
-                        byte type1 = paddedVoxels[p1_y * PaddedSize * PaddedSize + p1_z * PaddedSize + p1_x];
-                        byte type2 = paddedVoxels[p2_y * PaddedSize * PaddedSize + p2_z * PaddedSize + p2_x];
+                        // Culling check: face is visible if one side is solid and the other is air.
+                        // We also check that they are not the same type, allowing for different materials to have faces.
+                        bool solid1 = type1 != 0;
+                        bool solid2 = type2 != 0;
 
-                        bool solid1 = type1 != Air;
-                        bool solid2 = type2 != Air;
-
-                        mask[x[uAxis], x[vAxis]] = solid1 != solid2 ? (solid1 ? type1 : type2) : Air;
+                        if (solid1 == solid2)
+                        {
+                            mask[x[uAxis], x[vAxis]] = 0;
+                        }
+                        else if (solid1)
+                        {
+                            mask[x[uAxis], x[vAxis]] = type1;
+                        }
+                        else
+                        {
+                            mask[x[uAxis], x[vAxis]] = type2;
+                        }
                     }
                 }
 
-                for (int j = 0; j < Size; ++j)
+                ++x[(int)axis];
+
+                // 2. GENERATE MESH from the mask using the greedy algorithm
+                for (int j = 0; j < ChunkData.Size; ++j)
                 {
-                    for (int i = 0; i < Size;)
+                    for (int i = 0; i < ChunkData.Size;)
                     {
                         byte currentType = mask[i, j];
-                        if (currentType != Air)
+                        if (currentType != 0)
                         {
-                            int w; for (w = 1; i + w < Size && mask[i + w, j] == currentType; ++w) { }
-                            int h; bool done = false;
-                            for (h = 1; j + h < Size; ++h)
+                            // Find the width of the quad
+                            int w;
+                            for (w = 1; i + w < ChunkData.Size && mask[i + w, j] == currentType; ++w) { }
+
+                            // Find the height of the quad
+                            int h;
+                            bool done = false;
+                            for (h = 1; j + h < ChunkData.Size; ++h)
                             {
-                                for (int k = 0; k < w; ++k) { if (mask[i + k, j + h] != currentType) { done = true; break; } }
+                                for (int k = 0; k < w; ++k)
+                                {
+                                    if (mask[i + k, j + h] != currentType)
+                                    {
+                                        done = true;
+                                        break;
+                                    }
+                                }
                                 if (done) break;
                             }
 
+                            // Determine if this is a back-face, which needs opposite winding.
+                            // This relies on our earlier culling check logic.
                             x[uAxis] = i;
                             x[vAxis] = j;
+                            bool backFace = GetVoxel(x[0], x[1], x[2]) == 0;
 
-                            int p_x = x[0] + 1; int p_y = x[1] + 1; int p_z = x[2] + 1;
-                            bool isSolidBlock = paddedVoxels[p_y * PaddedSize * PaddedSize + p_z * PaddedSize + p_x] != Air;
-
-                            System.Drawing.Color color = currentType switch
+                            // Set color based on voxel type
+                            Color color = currentType switch
                             {
-                                Dirt => System.Drawing.Color.FromArgb(255, 139, 69, 19),
-                                Stone => System.Drawing.Color.FromArgb(255, 128, 128, 128),
-                                _ => System.Drawing.Color.HotPink
+                                1 => new Color("8b4513"), // Dirt
+                                2 => new Color("808080"), // Stone
+                                _ => Colors.HotPink
                             };
+                            st.SetColor(color);
 
-                            var du = new System.Numerics.Vector3();
-                            var dv = new System.Numerics.Vector3();
-                            switch (uAxis)
+                            // Define the 4 vertices of the quad in 3D space
+                            var v1 = new Vector3(); // Bottom-Left
+                            var v2 = new Vector3(); // Bottom-Right
+                            var v3 = new Vector3(); // Top-Left
+                            var v4 = new Vector3(); // Top-Right
+
+                            v1[(int)axis] = v2[(int)axis] = v3[(int)axis] = v4[(int)axis] = x[(int)axis];
+
+                            v1[uAxis] = i; v1[vAxis] = j;
+                            v2[uAxis] = i + w; v2[vAxis] = j;
+                            v3[uAxis] = i; v3[vAxis] = j + h;
+                            v4[uAxis] = i + w; v4[vAxis] = j + h;
+
+                            // Add the two triangles that make up the quad, with correct winding
+                            if (backFace)
                             {
-                                case 0: du.X = w; break;
-                                case 1: du.Y = w; break;
-                                case 2: du.Z = w; break;
-                            }
-                            switch (vAxis)
-                            {
-                                case 0: dv.X = h; break;
-                                case 1: dv.Y = h; break;
-                                case 2: dv.Z = h; break;
-                            }
-
-                            var v_bl = new System.Numerics.Vector3(x[0], x[1], x[2]);
-                            var v_br = v_bl + du;
-                            var v_tl = v_bl + dv;
-                            var v_tr = v_bl + du + dv;
-
-                            var normal = new System.Numerics.Vector3(dir[0], dir[1], dir[2]);
-
-                            if (isSolidBlock)
-                            {
-                                vertices.Add(v_bl); vertices.Add(v_tl); vertices.Add(v_br);
-                                vertices.Add(v_br); vertices.Add(v_tl); vertices.Add(v_tr);
-                                for (int m = 0; m < 6; m++) normals.Add(normal);
+                                // Counter-Clockwise winding
+                                st.AddVertex(v1); st.AddVertex(v3); st.AddVertex(v4);
+                                st.AddVertex(v1); st.AddVertex(v4); st.AddVertex(v2);
                             }
                             else
                             {
-                                vertices.Add(v_bl); vertices.Add(v_br); vertices.Add(v_tl);
-                                vertices.Add(v_tl); vertices.Add(v_br); vertices.Add(v_tr);
-                                for (int m = 0; m < 6; m++) normals.Add(-normal);
+                                // Clockwise winding
+                                st.AddVertex(v1); st.AddVertex(v4); st.AddVertex(v3);
+                                st.AddVertex(v1); st.AddVertex(v2); st.AddVertex(v4);
                             }
 
-                            for (int m = 0; m < 6; m++) colors.Add(color);
-                            for (int l = 0; l < h; ++l) for (int k = 0; k < w; ++k) mask[i + k, j + l] = 0;
+                            // Zero out the mask area we just covered
+                            for (int l = 0; l < h; ++l)
+                                for (int k = 0; k < w; ++k)
+                                    mask[i + k, j + l] = 0;
+
                             i += w;
                         }
-                        else i++;
+                        else
+                        {
+                            i++;
+                        }
                     }
                 }
             }
         }
-        return new ThreadSafeMeshResult(vertices, normals, colors);
+
+        st.GenerateNormals();
+        return st.Commit();
     }
 
     public byte GetVoxel(int x, int y, int z)
@@ -218,20 +183,29 @@ public partial class ChunkData : GodotObject
         {
             return _voxels[(y * Size * Size) + (z * Size) + x];
         }
-        var worldPos = new Godot.Vector3I(Position.X * Size + x, Position.Y * Size + y, Position.Z * Size + z);
-        var neighborChunkPos = new Godot.Vector3I(
+
+        var worldPos = new Vector3I(
+            Position.X * Size + x,
+            Position.Y * Size + y,
+            Position.Z * Size + z
+        );
+
+        var neighborChunkPos = new Vector3I(
             (int)Mathf.Floor(worldPos.X / (float)Size),
             (int)Mathf.Floor(worldPos.Y / (float)Size),
-            (int)Mathf.Floor(worldPos.Z / (float)Size));
+            (int)Mathf.Floor(worldPos.Z / (float)Size)
+        );
 
-        if (_world != null && _world.VoxelChunks.TryGetValue(neighborChunkPos, out ChunkData neighborChunk))
+        if (_world.VoxelChunks.TryGetValue(neighborChunkPos, out ChunkData neighborChunk))
         {
-            var localPos = new Godot.Vector3I(
+            var localPos = new Vector3I(
                 (worldPos.X % Size + Size) % Size,
                 (worldPos.Y % Size + Size) % Size,
-                (worldPos.Z % Size + Size) % Size);
+                (worldPos.Z % Size + Size) % Size
+            );
             return neighborChunk.GetVoxel(localPos.X, localPos.Y, localPos.Z);
         }
+
         return Air;
     }
 
@@ -242,18 +216,35 @@ public partial class ChunkData : GodotObject
 
     public void GenerateTerrain(FastNoiseLite noise)
     {
+        int stoneLevel = Size / 3;
+        int dirtLevel = (Size * 2) / 3;
+
         for (int x = 0; x < Size; x++)
+        {
             for (int z = 0; z < Size; z++)
             {
-                float noiseValue = noise.GetNoise2D(x + Position.X * Size, z + Position.Z * Size);
-                int surfaceHeight = (Size * 2) / 3 + (int)(noiseValue * 5);
+                int worldX = x + Position.X * Size;
+                int worldZ = z + Position.Z * Size;
+
+                float noiseValue = noise.GetNoise2D(worldX, worldZ);
+                int surfaceHeight = dirtLevel + (int)(noiseValue * 5);
+
                 for (int y = 0; y < Size; y++)
                 {
-                    if (y > surfaceHeight) SetVoxel(x, y, z, Air);
-                    else if (y < Size / 3) SetVoxel(x, y, z, Stone);
-                    else SetVoxel(x, y, z, Dirt);
+                    if (y > surfaceHeight)
+                    {
+                        SetVoxel(x, y, z, Air);
+                    }
+                    else if (y < stoneLevel)
+                    {
+                        SetVoxel(x, y, z, Stone);
+                    }
+                    else
+                    {
+                        SetVoxel(x, y, z, Dirt);
+                    }
                 }
             }
+        }
     }
 }
-
