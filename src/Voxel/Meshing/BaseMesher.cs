c021712e1b76bc9981a657c50c941c531fb01caf
znehAC <adrianocesar321@gmail.com>
2026-02-04 15:05:37 -0300

    refactor: remove godot code

--- src/Voxel/Meshing/BaseMesher.cs
+++
@@ -1,7 +0,0 @@
-using System.Threading;
-
-// Any class that can generate a mesh from voxel data must implement this interface.
-public interface BaseMesher
-{
-    MeshData GenerateMeshData(MeshJobData jobData, VoxelDefinition[] definitions, CancellationToken token);
-}
